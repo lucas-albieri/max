d00724911ec61f33dfb67ff1b6b7759c69461fbe
lucas-albieri <lucas.albisantos55@gmail.com>
2024-11-11 20:10:36 -0300

    Add initial components and layout for the application using Chakra UI

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import { Stack, Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Stack>
+      <Text>HELLO WORLD</Text>
+    </Stack>
+  );
+}
---
+++ src/app/teste/page.tsx
@@ -0,0 +1,12 @@
+import { Stack, Text } from "@chakra-ui/react";
+
+export default function Teste() {
+    return (
+        <Stack>
+            <Text>HELLO WORLD</Text>
+            <Text>
+                teste
+            </Text>
+        </Stack>
+    )
+}
