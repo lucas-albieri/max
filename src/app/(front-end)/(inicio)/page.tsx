7533972e65101102395da4ddbe55ac282fc88b35
lucas-albieri <lucas.albisantos55@gmail.com>
2025-01-10 15:45:00 -0300

    Refactor vertical banners into a separate constants file and update homepage component

--- src/app/(front-end)/(inicio)/page.tsx
+++ src/app/(front-end)/(inicio)/page.tsx
@@ -1,66 +1,11 @@
 import { Header } from "./_components/header";
 import bg from "../../../assets/images/welcome/bg.jpg";
 import { Plans } from "./_components/plans";
-import banner1 from "../../../assets/images/welcome/family.png"
-import banner2 from "../../../assets/images/welcome/reality_0.png"
-import banner3 from "../../../assets/images/welcome/novelas_1.jpg"
-import banner4 from "../../../assets/images/welcome/movies.jpg"
-import banner5 from "../../../assets/images/welcome/drama.jpg"
-import banner6 from "../../../assets/images/welcome/action.jpg"
-import banner7 from "../../../assets/images/welcome/comedy.jpg"
-import banner8 from "../../../assets/images/welcome/documentary_romario_o_cara.jpg"
-import banner9 from "../../../assets/images/welcome/Brasil.jpg"
+
 import { VerticalBanners } from "./_components/vertical-banners";
+import { verticalBanners } from "@/constants/vertical-banners";
 
 export default function Home() {
-
-  const verticalBanners = [
-    {
-      img: banner1.src,
-      title: "Peppa Pig",
-      type: "Crianças e Família"
-    },
-    {
-      img: banner2.src,
-      title: "Largados e Pelados",
-      type: "Reality"
-    },
-    {
-      img: banner3.src,
-      title: "Será Isso Amor?",
-      type: "Novelas"
-    },
-    {
-      img: banner4.src,
-      title: "GODZILLA VS KONG",
-      type: "Filmes"
-    },
-    {
-      img: banner5.src,
-      title: "A GUERRA DOS TRONOS",
-      type: "Drama"
-    },
-    {
-      img: banner6.src,
-      title: "THE LAST OF US",
-      type: "Ação"
-    },
-    {
-      img: banner7.src,
-      title: "THE BIG BANG THEORY",
-      type: "Comédia"
-    },
-    {
-      img: banner8.src,
-      title: "ROMÁRIO, O CARA",
-      type: "Documentários"
-    },
-    {
-      img: banner9.src,
-      title: "CHAMPIONS LEAGUE",
-      type: "Futebol"
-    }
-  ]
 
   return (
     <div
